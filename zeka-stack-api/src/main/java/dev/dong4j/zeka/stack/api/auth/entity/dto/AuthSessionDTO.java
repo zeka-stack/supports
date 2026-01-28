81c1a7fcd0ec239c598a131e1353a25942090303
dong4j <dong4j@gmail.com>
2026-01-28 20:55:44 +0800

    fix(zeka-stack-api): 更新应用代理默认启用状态并修复 AuthSessionDTO 注释格式
    - 将 application.yml 中代理功能默认启用状态从 false 改为 true, 确保代理功能默认开启
    - 修复 AuthSessionDTO.java 中注释格式, 移除冗余注释块并合并相关字段说明, 提升代码可读性

--- zeka-stack-api/src/main/java/dev/dong4j/zeka/stack/api/auth/entity/dto/AuthSessionDTO.java
+++ zeka-stack-api/src/main/java/dev/dong4j/zeka/stack/api/auth/entity/dto/AuthSessionDTO.java
@@ -1,13 +1,13 @@
 package dev.dong4j.zeka.stack.api.auth.entity.dto;
 
-import java.io.Serial;
-
 import dev.dong4j.zeka.kernel.common.base.BaseDTO;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+
+import java.io.Serial;
 
 /**
  * 登录会话响应
@@ -22,18 +22,11 @@ public class AuthSessionDTO extends BaseDTO<Long> {
     @Serial
     private static final long serialVersionUID = 1L;
 
-    /**
-     * 会话 token
-     *
-     * @see @Schema
-     */
+    /** 会话 token */
     @Schema(description = "会话 token")
     private String sessionToken;
 
-    /**
-     * 过期时间, 以毫秒为单位.
-     * 表示当前会话在该时间后失效.
-     */
+    /** 过期时间, 以毫秒为单位, 表示当前会话在该时间后失效 */
     @Schema(description = "过期时间(毫秒)")
     private Long expiresAt;
 
